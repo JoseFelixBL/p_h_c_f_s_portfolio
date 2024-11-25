9f49e57a5bdfc3cc32d23c6e04d649fde6daf44f
José Bello <jfbello.work@gmail.com>
2024-11-25 12:54:11 +0100

    Agregar load_dotenv y llave de SENDGRID a __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 import os
+from dotenv import load_dotenv
 
 
 def create_app():
     """Creamos nuestra aplicación Flask y le incorporamos los Blueprint necesarios"""
     app = Flask(__name__)
+
+    load_dotenv()
 
     # Usamos/Cogemos las configuraciónes de esta aplicación
     app.config.from_mapping(
